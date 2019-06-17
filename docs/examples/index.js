64cf54065a16d04c288c7afb89e34784444f76be
Asutosh <asutosh407@gmail.com>
2019-06-17 13:15:24 +0530

    testing1

---
+++ docs/examples/index.js
@@ -0,0 +1,30 @@
+console.log("test")
+
+async function test(){
+    if (window.ipfs && window.ipfs.enable) {
+      console.log("test1")
+        const ipfs = await window.ipfs.enable({commands: ['id','dag','version']})
+        const id= await ipfs.id()
+        console.log(ipfs)
+        console.log(id)
+        console.log(await ipfs.swarm.peers())
+        //const [{ hash }] = await ipfs.add(Buffer.from('i am 007'))
+        //const data = await ipfs.cat("QmUjC5vLEEbNVH2XGNaiNAf7F2yRjJhRQuWhWQfG98E8wj")
+        //console.log(hash)
+        //console.log(data.toString()) 
+        
+      } else {
+        // Fallback
+      }
+}
+
+test()
+// window.alert("test")
+// const node = new window.Ipfs()
+
+// node.on('ready', () => {
+//   // Ready to use!
+//   // See https://github.com/ipfs/js-ipfs#core-api
+// })
+
+
